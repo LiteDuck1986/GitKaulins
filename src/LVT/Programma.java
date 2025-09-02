a752e964105892a5d3adeff09fd9fd2bddeb7395
EmilsSmirnovs <emils2007smirnovs@gmail.com>
2025-09-02 12:15:37 +0300

    Ieviesta iespeja ievadit metienu skaitu

--- src/LVT/Programma.java
+++ src/LVT/Programma.java
@@ -1,15 +1,21 @@
 package LVT;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Programma {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis, reizes;
 		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kaulinu?");
+		reizes = scan.nextInt();
+		for (int i=1; i<=reizes; i++) {
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
 
 	}
-
+scan.close();
+	}
 }
